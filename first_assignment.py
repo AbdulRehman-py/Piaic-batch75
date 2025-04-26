e490fb629c2ac5350a9ad17270321b6e6a7eb119
Abdul Rehman <cotsrock30@gmail.com>
2025-04-26 10:55:14 +0500

    first assignment completed

---
+++ first_assignment.py
@@ -0,0 +1,76 @@
+# Piaic Python Assignment 1 - Batch 75
+
+length = int(15)
+width = int(10)
+
+area = length * width
+
+#print(area)
+
+
+# temperature = int(input("Enter temperature: "))
+
+# farhenheit = (temperature * 9/5) + 32
+
+# print("Temperature in farhenheit is: ", farhenheit)
+
+
+Math = 87
+Science = 92
+English = 78
+
+sum = Math + Science + English
+average = sum / 3
+#print("Average of three subjects is: ", average)
+
+
+Principal = 10000
+Rate = 0.05
+Time = 2
+
+simple_interest = (Principal * Rate * Time) / 100
+
+#print("Simple Interest is: ", simple_interest)
+
+height = 5.10
+
+foot1 = 12
+
+foot_inches = height * foot1
+
+#print("Height in inches is: ", foot_inches)
+
+radius = 7
+
+pi = 3.14
+
+# area of circle is given by the formula A = πr²
+
+area_of_circle = pi * (radius ** 2)
+
+#print("Area of circle is: ", area_of_circle)
+
+days  = 3
+
+hours = 24
+minutes = 60
+
+seconds = 60
+
+sum = days * hours
+
+sum1 = sum * minutes
+sum2 = sum1 * seconds
+
+#print("Seconds in 3 days is: ", sum2)
+
+# Volume of a cube
+side = 9
+volume_of_cube = side ** 3
+#print("Volume of the cube is: ", volume_of_cube)
+
+# Calculate BMI
+weight = 103  # in kilograms
+height = 1.75  # in meters
+bmi = weight / (height ** 2)
+#print("BMI (Body Mass Index) is: ", bmi)
